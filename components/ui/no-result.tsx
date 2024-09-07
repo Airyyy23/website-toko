5b4935e6f8cdcca88f16931d472a785bad8839ee
Airyyy23 <wayangseno123@gmail.com>
2024-09-07 23:52:47 +0700

    first commit

---
+++ components/ui/no-result.tsx
@@ -0,0 +1,9 @@
+const NoResult = () => {
+  return (
+    <div className="flex items-center justify-center h-full w-full text-neutral-500">
+        Tidak Ada Product
+    </div>
+  )
+}
+
+export default NoResult
